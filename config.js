c812684ea17d990399020ef12aa6097459dfde1a
Jozef Steinhübl <contact@xhyrom.dev>
2026-04-06 08:51:28 +0200

    feat: add gradle wrapper

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ module.exports = {
   ],
   labels: ["dependencies"],
   commitMessagePrefix: "build(deps): ",
-  allowedUnsafeExecutions: ["gradlew"],
+  allowedUnsafeExecutions: ["gradlew", "gradleWrapper"],
   onboarding: false,
   requireConfig: "optional",
 };
